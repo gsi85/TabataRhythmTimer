79ff4e6697c41a38140333b9ce7916232864acd7
Laszlo Sisa <sisa.laszlo@gmail.com>
2015-10-14 23:04:46 +0200

    adding media scanner functionality

--- app/src/main/java/com/sisa/tabata/media/scanner/MediaScanner.java
+++ app/src/main/java/com/sisa/tabata/media/scanner/MediaScanner.java
@@ -1,10 +1,17 @@
 package com.sisa.tabata.media.scanner;
 
-import com.google.inject.Singleton;
+import static android.media.MediaScannerConnection.OnScanCompletedListener;
+import static android.media.MediaScannerConnection.scanFile;
+import static com.sisa.tabata.validation.Assert.isInstanceOf;
+
+import com.sisa.tabata.ui.activity.MusicSelectActivity;
 
 import android.content.Context;
-import android.media.MediaScannerConnection;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Environment;
+
+import roboguice.inject.ContextSingleton;
 
 /**
  * Scanner for refreshing media store's meta data.
@@ -13,10 +20,12 @@ import android.os.Environment;
  *
  * @author Laszlo Sisa
  */
-@Singleton
-public class MediaScanner {
+@ContextSingleton
+public class MediaScanner implements OnScanCompletedListener {
 
     private static final String[] PATHS_TO_SCAN = new String[]{Environment.getExternalStorageDirectory().toString()};
+
+    private Context context;
 
     /**
      * Scans through the external storage to refresh data stored in media store's database.
@@ -24,7 +33,21 @@ public class MediaScanner {
      * @param context {@link Context}
      */
     public void scanFiles(final Context context) {
-        MediaScannerConnection.scanFile(context, PATHS_TO_SCAN, null, null);
+        this.context = context;
+        scanFile(context, PATHS_TO_SCAN, null, this);
+    }
+
+    @Override
+    public void onScanCompleted(final String path, final Uri uri) {
+        MusicSelectActivity musicSelectActivity = getContext();
+        Intent intent = musicSelectActivity.getIntent();
+        musicSelectActivity.finish();
+        musicSelectActivity.startActivity(intent);
+    }
+
+    private MusicSelectActivity getContext() {
+        isInstanceOf(MusicSelectActivity.class, context, "context is not a MusicSelectActivity!");
+        return (MusicSelectActivity) context;
     }
 
 }
