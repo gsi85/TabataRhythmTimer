b1e38a27ecf2f86bc6c8567d9f2863668b1fdeaf
Laszlo Sisa <sisa.laszlo@gmail.com>
2015-10-23 23:44:35 +0200

    storing loaded workout preference

--- app/src/main/java/com/sisa/tabata/dao/loader/WorkoutManager.java
+++ app/src/main/java/com/sisa/tabata/dao/loader/WorkoutManager.java
@@ -4,10 +4,14 @@ import static com.sisa.tabata.validation.Validation.empty;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.sisa.tabata.ApplicationContextProvider;
 import com.sisa.tabata.dao.service.WorkoutDao;
 import com.sisa.tabata.domain.Workout;
+import com.sisa.tabata.preferences.PreferencesType;
 import com.sisa.tabata.ui.domain.SerializedWorkout;
 import com.sisa.tabata.workout.transformer.SerializedWorkoutTransformer;
+
+import android.content.SharedPreferences;
 
 /**
  * Manages the currently loaded {@link Workout} instance.
@@ -17,6 +21,11 @@ import com.sisa.tabata.workout.transformer.SerializedWorkoutTransformer;
 @Singleton
 public class WorkoutManager {
 
+    private static final String PREFERENCES_FILE_NAME = "TabataPreferences";
+    private static final long NO_LOADED_WORKOUT_ID = -1;
+
+    @Inject
+    private ApplicationContextProvider applicationContextProvider;
     @Inject
     private SerializedWorkoutTransformer serializedWorkoutTransformer;
     @Inject
@@ -50,6 +59,11 @@ public class WorkoutManager {
      */
     public void loadFirstWorkoutInList() {
         workout = workoutDao.getAllWorkoutsSortedList().get(0);
+        //TODO: move this a common preference manager.
+        SharedPreferences settings = applicationContextProvider.getContext().getSharedPreferences(PREFERENCES_FILE_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putLong(PreferencesType.LOADED_WORKOUT_ID.name(), 0);
+        editor.apply();
     }
 
     /**
@@ -59,12 +73,35 @@ public class WorkoutManager {
      */
     public void setLoadedWorkoutById(long id) {
         workout = workoutDao.getWorkoutById(id);
+        //TODO: move this a common preference manager.
+        SharedPreferences settings = applicationContextProvider.getContext().getSharedPreferences(PREFERENCES_FILE_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putLong(PreferencesType.LOADED_WORKOUT_ID.name(), id);
+        editor.apply();
     }
 
     private void checkLoadWorkout() {
         if (empty(workout)) {
-            loadFirstWorkoutInList();
+            if (hasPreviouslyLoadedWorkout()) {
+                loadPreviouslySavedWorkout();
+            } else {
+                loadFirstWorkoutInList();
+            }
         }
+    }
+
+    private boolean hasPreviouslyLoadedWorkout() {
+        return getPreviouslyLoadedWorkoutId() != NO_LOADED_WORKOUT_ID;
+    }
+
+    private void loadPreviouslySavedWorkout() {
+        workout = workoutDao.getWorkoutById(getPreviouslyLoadedWorkoutId());
+    }
+
+    //TODO: move this a common preference manager.
+    private long getPreviouslyLoadedWorkoutId() {
+        SharedPreferences settings = applicationContextProvider.getContext().getSharedPreferences(PREFERENCES_FILE_NAME, 0);
+        return settings.getLong(PreferencesType.LOADED_WORKOUT_ID.name(), NO_LOADED_WORKOUT_ID);
     }
 
 }
